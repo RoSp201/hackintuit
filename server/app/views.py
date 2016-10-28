f1aebcce52c5905afe374165db56078fb736dcd6
Robert Spark <rspark@berkeley.edu>
2016-10-28 16:38:38 -0700

    rebuilding web app

---
+++ server/app/views.py
@@ -0,0 +1,16 @@
+
+from app import app
+from flask import render_template
+
+@app.route('/')
+@app.route('/index')
+def index():
+    user = {'nickname': 'Bob'}  # fake user
+    title = "title"
+    posts = [
+    	{
+	    	'author': {'nickname': 'John'}, 
+	        'body': 'Beautiful day in Portland!' 
+    	}
+    ]
+    return render_template("base.html", user=user, posts=posts, title=title)
